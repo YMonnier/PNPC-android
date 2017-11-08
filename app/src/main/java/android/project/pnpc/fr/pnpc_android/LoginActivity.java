2f57d22ae8f6f8f83378fa3515cdca61a21111e4
Stéphen BELLANGER <stephenbellanger@gmail.com>
2017-11-08 14:42:48 +0100

    Add LoginActivity + AA

---
+++ app/src/main/java/android/project/pnpc/fr/pnpc_android/LoginActivity.java
@@ -0,0 +1,10 @@
+package android.project.pnpc.fr.pnpc_android;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.login_activity)
+public class LoginActivity extends AppCompatActivity {
+
+}
