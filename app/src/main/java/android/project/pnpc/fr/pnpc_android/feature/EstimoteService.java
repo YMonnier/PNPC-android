4910bbc035ce0ce89605415f08c586828ef193aa
Stephen Bellanger <stephenbellanger@gmail.com>
2018-01-21 17:49:21 +0100

    Add Estimote Service

--- app/src/main/java/android/project/pnpc/fr/pnpc_android/feature/EstimoteService.java
+++ app/src/main/java/android/project/pnpc/fr/pnpc_android/feature/EstimoteService.java
@@ -1,37 +1,33 @@
 package android.project.pnpc.fr.pnpc_android.feature;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
+import android.app.Service;
 import android.content.Intent;
-import android.project.pnpc.fr.pnpc_android.R;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
 import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.estimote.coresdk.common.config.EstimoteSDK;
 import com.estimote.coresdk.common.requirements.SystemRequirementsChecker;
 import com.estimote.coresdk.observation.region.RegionUtils;
-import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
 import com.estimote.coresdk.observation.utils.Proximity;
 import com.estimote.coresdk.recognition.packets.EstimoteLocation;
 import com.estimote.coresdk.service.BeaconManager;
 
-import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.EService;
 
 import java.util.List;
-import java.util.UUID;
 
-@EActivity(R.layout.activity_estimote)
-public class EstimoteActivity extends AppCompatActivity {
+/**
+ * Created by stephen on 21/01/18.
+ */
 
-    @ViewById
-    TextView ok;
+@EService
+public class EstimoteService extends Service {
+
+    /**
+     * Tag used for Logger.
+     */
+    private static final String TAG = EstimoteService.class.getSimpleName();
 
     private String estimoteAppId = "pnpc-hiking-dzz";
 
@@ -41,9 +37,26 @@ public class EstimoteActivity extends AppCompatActivity {
 
     private BeaconManager beaconManager;
 
-    @AfterViews
-    public void init(){
-        SystemRequirementsChecker.checkWithDefaultDialogs(this);
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate");
+    }
+
+    /**
+     * Start the Estimote Service.
+     * Add the Location Update for Network and GPS Provider.
+     * If the the application does not follows permission, we
+     * ask the user to change it.
+     * @param intent
+     * @param startId
+     */
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+        Log.d(TAG, "onStart");
+
+        //SystemRequirementsChecker.checkWithDefaultDialogs(this);
         EstimoteSDK.initialize(getApplicationContext(), estimoteAppId, estimoteToken);
         run();
     }
@@ -63,18 +76,24 @@ public class EstimoteActivity extends AppCompatActivity {
                     boolean test = RegionUtils.computeProximity(beacon) == Proximity.NEAR;
                     Log.d("Test", "proximity : " + test + " ?");
                     if (beacon.id.toString().equals(pinkBeacon)){
-                        ok.setText("detected !!!!!");
+                        Log.e(TAG, "Detected !");
+                        //TODO : Send a HTTP request to server
                     }
                 }
             }
         });
     }
 
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
 
     @Override
-    protected void onDestroy() {
+    public void onDestroy() {
         super.onDestroy();
         beaconManager.disconnect();
     }
-
 }
